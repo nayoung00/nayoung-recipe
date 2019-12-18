20ad674922399a4d8ed5195cd21c021ede62c9a3
nayoung00 <invin1201@gmail.com>
2019-12-18 16:03:42 +0900

    프로젝트 초기화

---
+++ src/main/java/nayoung/cooknayoung/App.java
@@ -0,0 +1,11 @@
+// 프로젝트: 요리 레시피 사이트
+//
+package nayoung.cooknayoung;
+
+
+public class App {
+   
+    public static void main(String[] args) {
+        System.out.println("쿡나영의 레시피 세계에 오신걸 환영합니다!");
+    }
+}
