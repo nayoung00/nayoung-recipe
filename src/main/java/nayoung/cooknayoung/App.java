6c54f3dc8df02202ea27d09707724e0793fbdb1f
nayoung00 <invin1201@gmail.com>
2019-12-18 16:35:14 +0900

    222 출력 삭제

--- src/main/java/nayoung/cooknayoung/App.java
+++ src/main/java/nayoung/cooknayoung/App.java
@@ -8,6 +8,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("쿡나영의 레시피 세계에 오신걸 환영합니다!");
         System.out.println("111");
-        System.out.println("222");
+       
     }
 }
