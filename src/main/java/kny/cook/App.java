fc2c61139f9b6f1f695e6ddc09f1531b112fbd51
nayoung00 <invin1201@gmail.com>
2020-02-03 11:43:40 +0900

    바이너리 형식으로 입출력하는 방법

--- src/main/java/kny/cook/App.java
+++ src/main/java/kny/cook/App.java
@@ -1,21 +1,22 @@
 package kny.cook;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.Date;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
-import com.google.gson.Gson;
 import kny.cook.domain.Board;
 import kny.cook.domain.Member;
 import kny.cook.domain.Recipe;
@@ -120,11 +121,24 @@ public class App {
 
 
   private static void loadRecipeData() {
-    File file = new File("./recipe.json");
+    File file = new File("./recipe.data");
 
-    try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-      recipeList.addAll(Arrays.asList(new Gson().fromJson(in, Recipe[].class)));
+    try (DataInputStream in =
+        new DataInputStream(new BufferedInputStream(new FileInputStream(file)))) {
 
+      int size = in.readInt();
+      for (int i = 0; i < size; i++) {
+
+        Recipe recipe = new Recipe();
+        recipe.setNo(in.readInt());
+        recipe.setCook(in.readUTF());
+        recipe.setMaterial(in.readUTF());
+        recipe.setMethod(in.readUTF());
+        recipe.setExpense(in.readInt());
+        recipe.setTime(in.readInt());
+
+        recipeList.add(recipe);
+      }
       System.out.printf("총 %d 개의 요리 데이터를 로딩했습니다.\n", recipeList.size());
 
     } catch (IOException e) {
@@ -133,45 +147,93 @@ public class App {
   }
 
   private static void saveRecipeData() {
-    File file = new File("./recipe.json");
-    try (BufferedWriter out = new BufferedWriter(new FileWriter(file))) {
-      out.write(new Gson().toJson(recipeList));
-
+    File file = new File("./recipe.data");
+    try (DataOutputStream out =
+        new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file)))) {
+      out.writeInt(recipeList.size());
+      for (Recipe recipe : recipeList) {
+        out.writeInt(recipe.getNo());
+        out.writeUTF(recipe.getCook());
+        out.writeUTF(recipe.getMaterial());
+        out.writeUTF(recipe.getMethod());
+        out.writeInt(recipe.getExpense());
+        out.writeInt(recipe.getTime());
+      }
       System.out.printf("총 %d 개의 요리 데이터를 저장했습니다.", recipeList.size());
-    } catch (IOException e) {
+    } catch (Exception e) {
       System.out.println("파일 쓰기 중 오류 발생! - " + e.getMessage());
     }
   }
 
   private static void loadBoardData() {
-    File file = new File("./board.json");
+    File file = new File("./board.data");
     Scanner dataScan = null;
 
-    try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-      boardList.addAll(Arrays.asList(new Gson().fromJson(in, Board[].class)));
+    try (DataInputStream in =
+        new DataInputStream(new BufferedInputStream(new FileInputStream(file)))) {
+      int size = in.readInt();
+
+      for (int i = 0; i < size; i++) {
+        Board board = new Board();
+        board.setNo(in.readInt());
+        board.setTitle(in.readUTF());
+        board.setDate(Date.valueOf(in.readUTF()));
+        board.setViewCount(in.readInt());
+        String writer = in.readUTF();
+
+        if (writer.length() > 0) {
+          board.setWriter(writer);
+        }
+        boardList.add(board);
+
+      }
       System.out.printf("총 %d 개의 게시글 데이터를 로딩했습니다.\n", boardList.size());
-    } catch (IOException e) {
+    } catch (
+
+    IOException e) {
       System.out.println("파일 읽기 중 오류 발생! -" + e.getMessage());
     }
   }
 
   private static void saveBoardData() {
-    File file = new File("./board.json");
+    File file = new File("./board.data");
 
-    try (BufferedWriter out = new BufferedWriter(new FileWriter(file))) {
-      out.write(new Gson().toJson(boardList));
+    try (DataOutputStream out =
+        new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file)))) {
+      out.writeInt(boardList.size());
+      for (Board board : boardList) {
+        out.writeInt(board.getNo());
+        out.writeUTF(board.getTitle());
+        out.writeUTF(board.getDate().toString());
+        out.writeInt(board.getViewCount());
+        out.writeUTF(board.getWriter() == null ? "" : board.getWriter());
+      }
+
       System.out.printf("총 %d 개의 게시글 데이터를 저장했습니다.", boardList.size());
-    } catch (IOException e) {
+
+
+    } catch (Exception e) {
       System.out.println("파일 쓰기 중 오류 발생! - " + e.getMessage());
     }
   }
 
 
   private static void loadMemberData() {
-    File file = new File("./member.json");
+    File file = new File("./member.data");
+    try (DataInputStream in =
+        new DataInputStream(new BufferedInputStream(new FileInputStream(file)))) {
+      int size = in.readInt();
+      for (int i = 0; i < size; i++) {
+        Member member = new Member();
+        member.setNo(in.readInt());
+        member.setName(in.readUTF());
+        member.setEmail(in.readUTF());
+        member.setPassword(in.readUTF());
+        member.setPhoto(in.readUTF());
+        member.setRegisteredDate(Date.valueOf(in.readUTF()));
 
-    try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-      memberList.addAll(Arrays.asList(new Gson().fromJson(in, Member[].class)));
+        memberList.add(member);
+      }
       System.out.printf("총 %d 개의 회원 데이터를 로딩했습니다.\n", memberList.size());
 
     } catch (IOException e) {
@@ -180,10 +242,19 @@ public class App {
   }
 
   private static void saveMemberData() {
-    File file = new File("./member.json");
+    File file = new File("./member.data");
 
-    try (BufferedWriter out = new BufferedWriter(new FileWriter(file))) {
-      out.write(new Gson().toJson(memberList));
+    try (DataOutputStream out =
+        new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file)))) {
+      out.writeInt(memberList.size());
+      for (Member member : memberList) {
+        out.writeInt(member.getNo());
+        out.writeUTF(member.getName());
+        out.writeUTF(member.getEmail());
+        out.writeUTF(member.getPassword());
+        out.writeUTF(member.getPhoto());
+        out.writeUTF(member.getRegisteredDate().toString());
+      }
       System.out.printf("총 %d 개의 회원 데이터를 저장했습니다.", memberList.size());
     } catch (IOException e) {
       System.out.println("파일 쓰기 중 오류 발생! -" + e.getMessage());
