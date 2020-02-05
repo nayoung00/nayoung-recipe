b94639157cd6367fa7eaa12ca7af28f60082cf7e
nayoung00 <invin1201@gmail.com>
2020-02-06 08:47:22 +0900

    클라이언트로부터 메시지를 수신 받고 응답하기

--- README.md
+++ README.md
@@ -1,36 +1,16 @@
-# 26_1 - 자바 서버 프로젝트 만들기
+# 26_2 - 클라이언트로부터 메시지를 수신 받고 응답하기
 
 ## 실습 소스 및 결과
 
-- src/main/java/kny/cook/ServerApp.java 추가
+- src/main/java/kny/cook/ServerApp.java 변경
 
 ## 실습  
 
-### 작업 1: 프로젝트 폴더를 생성하라.
+### 작업 1: 클라이언트 연결을 대기하라.
 
-- 'recipe-server' 디렉토리를 생성한다.
+- ServerApp.java 변경
 
-### 작업 2: 프로젝트 폴더에 자바 프로젝트로 만들라.
+### 작업 2: 클라이언트가 보낸 메시지를 읽고 응답 메시지를 전송하라.
 
-- '$ gradle init' 실행
-
-### 작업 3: 이클립스 IDE로 임포트 하라.
-
-- 'build.gradle' 변경
-  - 'eclipse' gradle 플러그인을 추가한다.
-  - 'javaCompile'을 설정한다.
-- '$ gradle eclipse' 실행
-  - gradle을 실행하여 이클립스 설정 파일을 생성한다.
-- 이클립스에서 프로젝트 폴더를 임포트한다.
-
-### 작업 4: 프로젝트 시작 클래스를 변경하라.
-
-- 'ServerApp.java' 생성
-  - 기존의 'App.java'의 클래스 이름을 변경한다.
-  - 소스 코드를 정리한다.
-  - '서버 레시피 관리 시스템입니다' 문구를 출력한다.
-- 'src/test/java/ServerAppTest.java' 생성
-  - 기존의 'AppTest.java'의 클래스 이름을 변경한다.
-  - 소스 코드를 정리한다.
-- ServerApp.java 를 실행하여 결과를 확인한다.    
+- ServerApp.java 변경
 
--- src/main/java/kny/cook/ServerApp.java
+++ src/main/java/kny/cook/ServerApp.java
@@ -1,9 +1,51 @@
 // LMS 서버
 package kny.cook;
 
-public class ServerApp {
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class ServerApp {
   public static void main(String[] args) {
     System.out.println("서버 레시피 관리 시스템입니다.");
+
+    try (
+
+        ServerSocket serverSocket = new ServerSocket(9999)) {
+      System.out.println("클라이언트 연결 대기중...");
+
+      while (true) {
+        Socket socket = serverSocket.accept();
+        System.out.println("클라이언트와 연결되었음.");
+
+        processRequest(socket);
+        System.out.println("--------------------------");
+      }
+    } catch (IOException e) {
+      System.out.println("서버 준비 중 오류 발생!");
+      return;
+    }
+  }
+
+  static void processRequest(Socket clientSocket) {
+    try (Socket socket = clientSocket;
+
+        Scanner in = new Scanner(socket.getInputStream());
+        PrintStream out = new PrintStream(socket.getOutputStream())) {
+
+      System.out.println("통신을 위한 입출력 스트림을 준비하였음!");
+
+      String message = in.nextLine();
+      System.out.println("클라이언트가 보낸 메시지를 수신하였음!");
+      System.out.println("클라이언트: " + message);
+
+      out.println("Hi(나영)");
+      System.out.println("클라이언트로 메시지를 전송하였음.");
+    } catch (IOException e) {
+      System.out.println("예외 발생: ");
+      e.printStackTrace();
+    }
   }
 }
