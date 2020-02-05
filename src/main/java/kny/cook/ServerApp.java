b6092e2558692f2f10bbd83865ca53ef8194783d
nayoung00 <invin1201@gmail.com>
2020-02-05 20:04:52 +0900

    자바 서버 프로젝트 만들기

---
+++ src/main/java/kny/cook/ServerApp.java
@@ -0,0 +1,9 @@
+// LMS 서버
+package kny.cook;
+
+public class ServerApp {
+
+  public static void main(String[] args) {
+    System.out.println("서버 레시피 관리 시스템입니다.");
+  }
+}
---
+++ src/test/java/kny/cook/ServerAppTest.java
@@ -0,0 +1,11 @@
+package kny.cook;
+
+import org.junit.Test;
+
+public class ServerAppTest {
+  @Test
+  public void testAppHasAGreeting() {
+    // ServerApp classUnderTest = new ServerApp();
+    // assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+  }
+}
