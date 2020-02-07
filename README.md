0400efc3a8cfde2316a1bf430d1247f2c7927ae2
nayoung00 <invin1201@gmail.com>
2020-02-07 18:31:44 +0900

    클라이언트의 데이터 관리 요청을 처리하기

--- README.md
+++ README.md
@@ -1,26 +1,52 @@
-# 26_3 - LMS 관리 데이터를 파일에서 로딩하고, 파일로 저장하기 
+# 26_4 - 클라이언트의 데이터 관리 요청을 처리하기 
 
 
 ## 실습 소스 및 결과
 
-- src/main/java/kny/cook/domain 패키지 생성
-- src/main/java/kny/cook/domain/Board.java 추가
-- src/main/java/kny/cook/domain/Recipe.java 추가
-- src/main/java/kny/cook/domain/Member.java 추가
-- src/main/java/kny/cook/context 패키지 생성
-- src/main/java/kny/cook/context/ApplicationContextListener.java 추가
-- src/main/java/kny/cook/DataLoaderListener.java 추가
 - src/main/java/kny/cook/ServerApp.java 변경
 
-## 실습  
 
-### 훈련 1: 25번 프로젝트에서 데이터를 저장하고 로딩하는 코드 가져오기.
+### 작업 1: 서비스를 시작할 때 클라이언트의 연결을 기다리는 코드를 추가하기.
 
-- kny.cook.domain 패키지를 생성한다.
-- kny.cook.domain.* 클래스를 가져온다.
-- kny.cook.context 패키지를 생성한다.
-- kny.cook.context.ApplicationContextListener 를 가져온다.
-- kny.cook.DataLoaderListener 를 가져온다.
 - ServerApp.java 변경
-  - 옵저버를 등록하고, 호출하는 코드를 적용한다.
+  - ServerSocket을 준비한다.
+  - 클라이언트 연결을 준비한다.
+  
+### 작업 2: 클라이언트의 게시물 목록 요청(/board/list)을 처리하기.
 
+- ServerApp.java 변경
+  - processRequest() 메서드 변경
+- ServerAppTest.java 추가
+  - 서버의 응답 기능을 테스트 한다.
+- Board.java 변경
+  - 통신 테스트 할 때 게시물 필드 정보를 확인할 수 있도록 toString()을 오버라이딩 한다.
+
+### 작업 3: 클라이언트의 게시물 등록 요청(/board/add)을 처리하기.
+
+- ServerApp.java 변경
+  - processRequest() 메서드 변경
+
+### 작업 4: 클라리언트의 게시물 조회 요청(/board/detail)을 처리하기.
+
+- ServerApp.java 변경
+  - processRequest() 메서드 변경
+  
+### 작업 5: 클라리언트의 게시물 변경 요청(/board/update)을 처리하기.
+
+- ServerApp.java 변경
+  - processRequest() 메서드 변경  
+  
+### 작업 6: 클라리언트의 게시물 삭제 요청(/board/delete)을 처리하기.
+
+- ServerApp.java 변경
+  - processRequest() 메서드 변경  
+
+### 작업 7: 클라리언트의 수업 관리 요청(/lesson/*)을 처리하기.
+
+- ServerApp.java 변경
+  - processRequest() 메서드 변경  
+  
+### 작업 8: 클라리언트의 회원 관리 요청(/member/*)을 처리하기.
+
+- ServerApp.java 변경
+  - processRequest() 메서드 변경  
--- src/main/java/kny/cook/ServerApp.java
+++ src/main/java/kny/cook/ServerApp.java
@@ -1,14 +1,14 @@
 // LMS 서버
 package kny.cook;
 
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.Set;
 import kny.cook.context.ApplicationContextListener;
 import kny.cook.domain.Board;
@@ -38,16 +38,345 @@ public class ServerApp {
       listener.contextDestroyed(context);
   }
 
-  @SuppressWarnings("unchecked")
   public void service() {
 
     notifyApplicationInitialized();
 
-    List<Board> boardList = (List<Board>) context.get("boardList");
-    List<Recipe> recipeList = (List<Recipe>) context.get("recipeList");
-    List<Member> memberList = (List<Member>) context.get("memberList");
+    try (ServerSocket serverSocket = new ServerSocket(9999)) {
+      System.out.println("클라이언트와 연결 대기 중...");
+
+      while (true) {
+        Socket socket = serverSocket.accept();
+        System.out.println("클라이언트와 연결 되었음!");
+
+        if (processRequest(socket) == 9) {
+          break;
+        }
+      }
+
+    } catch (Exception e) {
+      System.out.println("서버 준비 중 오류 발생!");
+    }
 
     notifyApplicationDestroyed();
+  }
+
+  @SuppressWarnings("unchecked")
+  int processRequest(Socket clientSocket) {
+    try (Socket socket = clientSocket;
+
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream())) {
+
+      System.out.println("통신을 위한 입출력 스트림을 준비하였음!");
+
+      while (true) {
+        String request = in.readUTF();
+        System.out.println("클라이언트가 보낸 메시지를 수신하였음!");
+
+        if (request.equals("quit")) {
+          out.writeUTF("OK");
+          out.flush();
+          break;
+        }
+
+        if (request.equals("/server/stop")) {
+          out.writeUTF("OK");
+          out.flush();
+          return 9;
+        }
+        List<Board> boards = (List<Board>) context.get("boardList");
+        List<Recipe> recipes = (List<Recipe>) context.get("recipeList");
+        List<Member> members = (List<Member>) context.get("memberList");
+
+        if (request.equals("/recipe/list")) {
+          out.writeUTF("OK");
+          out.reset();
+          out.writeObject(recipes);
+        } else if (request.equals("/recipe/add")) {
+          try {
+            Recipe recipe = (Recipe) in.readObject();
+
+            int i = 0;
+            for (; i < recipes.size(); i++) {
+              if (recipes.get(i).getNo() == recipe.getNo()) {
+                break;
+              }
+            }
+            if (i == recipes.size()) {
+              recipes.add(recipe);
+              out.writeUTF("OK");
+            } else {
+              out.writeUTF("FAIL");
+              out.writeUTF("같은 번호의 레시피가 있습니다.");
+            }
+          } catch (Exception e) {
+            out.writeUTF("FAIL");
+            out.writeUTF(e.getMessage());
+          }
+        } else if (request.equals("/recipe/delete")) {
+          try {
+            int no = in.readInt();
+
+            int index = -1;
+            for (int i = 0; i < recipes.size(); i++) {
+              if (recipes.get(i).getNo() == no) {
+                index = i;
+                break;
+              }
+            }
+            if (index != -1) {
+              recipes.remove(index);
+              out.writeUTF("OK");
+            } else {
+              out.writeUTF("FAIL");
+              out.writeUTF("해당 번호의 레시피가 없습니다.");
+            }
+          } catch (Exception e) {
+            out.writeUTF("FAIL");
+            out.writeUTF(e.getMessage());
+          }
+        } else if (request.equals("/recipe/detail")) {
+          try {
+            int no = in.readInt();
+
+            Recipe recipe = null;
+            for (Recipe r : recipes) {
+              if (r.getNo() == no) {
+                recipe = r;
+                break;
+              }
+            }
+            if (recipe != null) {
+              out.writeUTF("OK");
+              out.writeObject(recipe);
+            } else {
+              out.writeUTF("FAIL");
+              out.writeUTF("해당 번호의 레시피가 없습니다.");
+            }
+          } catch (Exception e) {
+            out.writeUTF("FAIL");
+            out.writeUTF(e.getMessage());
+          }
+        } else if (request.equals("/recipe/update")) {
+          try {
+            Recipe recipe = (Recipe) in.readObject();
+
+            int index = -1;
+            for (int i = 0; i < recipes.size(); i++) {
+              if (recipes.get(i).getNo() == recipe.getNo()) {
+                index = i;
+                break;
+              }
+            }
+            if (index != -1) {
+              recipes.set(index, recipe);
+              out.writeUTF("OK");
+            } else {
+              out.writeUTF("FAIL");
+              out.writeUTF("해당 번호의 레시피가 없습니다.");
+            }
+          } catch (Exception e) {
+            out.writeUTF("FAIL");
+            out.writeUTF(e.getMessage());
+          }
+        } else if (request.equals("/member/list")) {
+          out.writeUTF("OK");
+          out.reset();
+          out.writeObject(members);
+
+        } else if (request.equals("/member/add")) {
+          try {
+            Member member = (Member) in.readObject();
+
+            int i = 0;
+            for (; i < members.size(); i++) {
+              if (members.get(i).getNo() == member.getNo()) {
+                break;
+              }
+            }
+            if (i == members.size()) {
+              members.add(member);
+              out.writeUTF("OK");
+            } else {
+              out.writeUTF("FAIL");
+              out.writeUTF("같은 번호의 회원이 있습니다.");
+            }
+          } catch (Exception e) {
+            out.writeUTF("FAIL");
+            out.writeUTF(e.getMessage());
+          }
+        } else if (request.equals("/member/delete")) {
+          try {
+            int no = in.readInt();
+
+            int index = -1;
+            for (int i = 0; i < members.size(); i++) {
+              if (members.get(i).getNo() == no) {
+                index = i;
+                break;
+              }
+            }
+            if (index != -1) {
+              members.remove(index);
+              out.writeUTF("OK");
+            } else {
+              out.writeUTF("FAIL");
+              out.writeUTF("해당 번호의 회원이 없습니다.");
+            }
+          } catch (Exception e) {
+            out.writeUTF("FAIL");
+            out.writeUTF(e.getMessage());
+          }
+        } else if (request.equals("/member/detail")) {
+          try {
+            int no = in.readInt();
+
+            Member member = null;
+            for (Member m : members) {
+              if (m.getNo() == no) {
+                member = m;
+                break;
+              }
+            }
+            if (member != null) {
+              out.writeUTF("OK");
+              out.writeObject(member);
+            } else {
+              out.writeUTF("FAIL");
+              out.writeUTF("해당 번호의 회원이 없습니다.");
+            }
+          } catch (Exception e) {
+            out.writeUTF("FAIL");
+            out.writeUTF(e.getMessage());
+          }
+        } else if (request.equals("/member/update")) {
+          try {
+            Member member = (Member) in.readObject();
+
+            int index = -1;
+            for (int i = 0; i < members.size(); i++) {
+              if (members.get(i).getNo() == member.getNo()) {
+                index = i;
+                break;
+              }
+            }
+            if (index != -1) {
+              members.set(index, member);
+              out.writeUTF("OK");
+            } else {
+              out.writeUTF("FAIL");
+              out.writeUTF("해당 번호의 회원이 없습니다.");
+            }
+          } catch (Exception e) {
+            out.writeUTF("FAIL");
+            out.writeUTF(e.getMessage());
+          }
+        } else if (request.equals("/board/list")) {
+          out.writeUTF("OK");
+          out.reset();
+          out.writeObject(boards);
+
+        } else if (request.equals("/board/add")) {
+          try {
+            Board board = (Board) in.readObject();
+
+            int i = 0;
+            for (; i < boards.size(); i++) {
+              if (boards.get(i).getNo() == board.getNo()) {
+                break;
+              }
+            }
+            if (i == boards.size()) {
+              boards.add(board);
+              out.writeUTF("OK");
+            } else {
+              out.writeUTF("FAIL");
+              out.writeUTF("같은 번호의 게시물이 있습니다.");
+            }
+          } catch (Exception e) {
+            out.writeUTF("FAIL");
+            out.writeUTF(e.getMessage());
+          }
+        } else if (request.equals("/board/delete")) {
+          try {
+            int no = in.readInt();
+
+            int index = -1;
+            for (int i = 0; i < boards.size(); i++) {
+              if (boards.get(i).getNo() == no) {
+                index = i;
+                break;
+              }
+            }
+            if (index != -1) {
+              boards.remove(index);
+              out.writeUTF("OK");
+            } else {
+              out.writeUTF("FAIL");
+              out.writeUTF("해당 번호의 게시물이 없습니다.");
+            }
+          } catch (Exception e) {
+            out.writeUTF("FAIL");
+            out.writeUTF(e.getMessage());
+          }
+        } else if (request.equals("/board/detail")) {
+          try {
+            int no = in.readInt();
+
+            Board board = null;
+            for (Board b : boards) {
+              if (b.getNo() == no) {
+                board = b;
+                break;
+              }
+            }
+            if (board != null) {
+              out.writeUTF("OK");
+              out.writeObject(board);
+            } else {
+              out.writeUTF("FAIL");
+              out.writeUTF("해당 번호의 게시물이 없습니다.");
+            }
+          } catch (Exception e) {
+            out.writeUTF("FAIL");
+            out.writeUTF(e.getMessage());
+          }
+        } else if (request.equals("/board/update")) {
+          try {
+            Board board = (Board) in.readObject();
+
+            int index = -1;
+            for (int i = 0; i < boards.size(); i++) {
+              if (boards.get(i).getNo() == board.getNo()) {
+                index = i;
+                break;
+              }
+            }
+            if (index != -1) {
+              boards.set(index, board);
+              out.writeUTF("OK");
+            } else {
+              out.writeUTF("FAIL");
+              out.writeUTF("해당 번호의 게시물이 없습니다.");
+            }
+          } catch (Exception e) {
+            out.writeUTF("FAIL");
+            out.writeUTF(e.getMessage());
+          }
+        } else {
+          out.writeUTF("FAIL");
+          out.writeUTF("요청한 명령을 처리할 수 없습니다.");
+        }
+        out.flush();
+      }
+      System.out.println("클라이언트로 메시지를 전송하였음.");
+      return 0;
+    } catch (Exception e) {
+      System.out.println("예외 발생: ");
+      e.printStackTrace();
+      return -1;
+    }
   }
 
   public static void main(String[] args) {
@@ -57,24 +386,5 @@ public class ServerApp {
     app.addApplicationContextListener(new DataLoaderListener());
     app.service();
   }
-
-  static void processRequest(Socket clientSocket) {
-    try (Socket socket = clientSocket;
-
-        Scanner in = new Scanner(socket.getInputStream());
-        PrintStream out = new PrintStream(socket.getOutputStream())) {
-
-      System.out.println("통신을 위한 입출력 스트림을 준비하였음!");
-
-      String message = in.nextLine();
-      System.out.println("클라이언트가 보낸 메시지를 수신하였음!");
-      System.out.println("클라이언트: " + message);
-
-      out.println("Hi(나영)");
-      System.out.println("클라이언트로 메시지를 전송하였음.");
-    } catch (IOException e) {
-      System.out.println("예외 발생: ");
-      e.printStackTrace();
-    }
-  }
 }
+
